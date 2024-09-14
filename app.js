8e426269b418b8cd2acea967daeb2f8a795aa92f
codereeyo <riomndres@gmail.com>
2024-09-14 14:34:02 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const menuList = document.getElementById("menuList");
+    const menu = document.getElementById("menu");
+    
+    menuList.style.maxHeight = "0px";
+    
+    menu.addEventListener("click", function () {
+        if(menuList.style.maxHeight == "0px"){
+            menuList.style.maxHeight = "430px";
+        }
+        else{
+            menuList.style.maxHeight = "0px";
+        }
+    })
+    
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });  
+
+    // SCROLL REVEAL 
+    const scrollRevealOption = {
+        distance: "200px",
+        origin: "bottom",
+        duration: 1000,
+    }
+
+    ScrollReveal().reveal(".hero-banner .right-column", {
+        ...scrollRevealOption,
+        origin: "right",
+        duration: 1000,
+    });
+
+    ScrollReveal().reveal(".hero-banner .left-column h1", {
+        ...scrollRevealOption,
+        duration: 1500,
+    });
+
+    ScrollReveal().reveal(".hero-banner .left-column p", {
+        ...scrollRevealOption,
+        duration: 2000,
+    });
+
+    ScrollReveal().reveal(".proj-container-image-left", {
+        ...scrollRevealOption,
+        origin: "right",
+    });
+    ScrollReveal().reveal(".proj-container-image-right", {
+        ...scrollRevealOption,
+        origin: "left",
+    });
+})
